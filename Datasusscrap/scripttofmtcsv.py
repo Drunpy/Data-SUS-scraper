04ce44fe263bc479d5598657a692b406271a261f
Lorran Rosa <lorransilva1998@gmail.com>
2018-06-16 05:40:12 -0300

    Add comments

--- Datasusscrap/scripttofmtcsv.py
+++ Datasusscrap/scripttofmtcsv.py
@@ -1,3 +1,7 @@
+'''
+    Data SUS do not ofers the option to download CSV, the closer is separated by ';'.
+    This code simply overcome this insue.
+'''
 with open('Data2000 txt.txt', 'r') as txt:
     file = txt.read().replace(';', ',')
 with open('out2000fmt.csv', 'w') as out:
